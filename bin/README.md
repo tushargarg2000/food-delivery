89bfab917987fa1f001e8124899eb02b6fba3f56
notsoloud <kanmohan2911@gmail.com>
2023-01-28 12:47:45 +0530

    Sol

--- bin/README.md
+++
@@ -1,1 +0,0 @@
-# Food-Delivery-Web-App-BackEnd-ScpringBoot
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -18,6 +18,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -28,8 +29,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class TestCases {
 
 	@InjectMocks
